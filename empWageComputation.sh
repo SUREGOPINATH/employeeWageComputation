23d26c9837b69aa7bd08090a0e4e2d5109d40835
SURE GOPINATH <suregopinath@gmail.com>
2020-10-05 17:17:26 +0530

    Employee Attendece problem is done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,15 @@
+#!/bin/bash -x
 echo "welcome to program";
+
+##code of employee Attendece
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
+then
+        echo "Employee is Present";
+else
+        echo "Employee is Abscent";
+fi
+

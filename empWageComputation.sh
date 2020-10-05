bae97108518c03bc51b48a073d295b925c4bcaf3
SURE GOPINATH <suregopinath@gmail.com>
2020-10-05 22:05:17 +0530

    employee wages till working hours is done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -95,3 +95,33 @@ do
         totalSalary=$(($totalSalary+$salary));
 done
 
+##code of employee wages till working hours
+
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+        totalWorkingDays=$(($totalWorkingDays+1))
+        empCheck=$((RANDOM%3))
+        case $empCheck in
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        totalSalary=$(($totalSalary+($totalEmpHrs*$empRatePerHr)))
+done
+

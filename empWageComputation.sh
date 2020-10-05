357c1740eb615ae303f47166c4d906b775c3ce5d
SURE GOPINATH <suregopinath@gmail.com>
2020-10-05 21:32:09 +0530

    employee wages for month is done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -69,3 +69,29 @@ esac
 
 salary=$(($empHrs*$empRatePerHr))
 
+##code of employee for month
+
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+totalSalary=0
+numWorkingDays=30
+
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+        empCheck=$((RANDOM%3));
+        case $empCheck in
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        salary=$(($empHrs*$empRatePerHr));
+        totalSalary=$(($totalSalary+$salary));
+done
+
